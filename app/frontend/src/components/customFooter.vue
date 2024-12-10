bc2c08a17d77f0f82968b7bf2da1e33e78841429
Andreas Gabriel Vannas <jaanus.maddison@gmail.com>
2024-12-10 21:02:03 +0200

    feat: Add copyright footer

---
+++ app/frontend/src/components/customFooter.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+    <footer class="bg-body-tertiary text-center text-lg-start custom-footer">
+        <div class="text-center p-3">
+            <p class="text-body">Copyright (c) 2024 WorldSkills Estonia</p>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+.custom-footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: auto; 
+}
+</style>
