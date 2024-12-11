7da3541ef488ca697574e900d7d31d7d861562a4
Andreas Gabriel Vannas <jaanus.maddison@gmail.com>
2024-12-11 21:42:51 +0200

    fix: change 404 page to look nicer

--- app/frontend/src/views/notFoundView.vue
+++ app/frontend/src/views/notFoundView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import notFound from '@/assets/media/404.gif'
-import logo from '@/assets/media/logo.svg';
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
@@ -9,19 +9,17 @@ import logo from '@/assets/media/logo.svg';
             <div class="row">
                 <div class="col">
                     <div class="text-center">
-                        <div class="four_zero_four_bg">
-                            <img alt="Credit: Andreas Gabriel Vannas" :src="logo" class="pt-2" width="125" height="125" />
-                            <h1 class="text-center">404</h1>
-                        </div>
-                        <div class="contant_box_404">
-                            <h3>
-                                Page not Found.
-                            </h3>
-                            <p>Something went wrong.</p>
-                            <div width="auto" class="row d-flex justify-content-center">
-                                <img alt="Caveman by Markus Magnusson" :src="notFound" class="image-width"/>
-                                <a href="https://www.instagram.com/motionmarkus/">Caveman by Markus Magnusson</a>
-                            </div>
+                
+                        <h1 class="text-center">404</h1>
+                        <h3>
+                            Page not Found.
+                        </h3>
+                        <p>Something went wrong.</p>
+                        <RouterLink to="home" class="btn btn-dark">Back Home</RouterLink>
+                        <a href="https://www.instagram.com/motionmarkus/" class="btn btn-outline-dark m-3 set-item-top" target="_blank" rel="noopener noreferrer">Caveman by Markus Magnusson</a>
+
+                        <div class="allign-center">
+                            <img alt="Caveman by Markus Magnusson" :src="notFound" class="set-item-bottom image-width"/>
                         </div>
                     </div>
                 </div>
