03394bae3abc30249c8268d1e10783049d60be93
Andreas Gabriel Vannas <jaanus.maddison@gmail.com>
2024-12-12 22:16:12 +0200

    feat: add a dummy landing page

--- app/frontend/src/views/homeView.vue
+++ app/frontend/src/views/homeView.vue
@@ -1,7 +1,29 @@
 <script setup>
-import { RouterView } from 'vue-router';
+import logo from "@/assets/media/logo.svg";
+
+// Get user state
+//import { loggedInUsername } from "@/components/login/LoginPage.vue";
+//const { authUser } = loggedInUsername();
 
 </script>
 
 <template>
+    
+    <div class="container p-3">
+        <div
+            class="alert alert-primary alert-dismissible fade show"
+            role="alert"
+        >
+            <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="alert"
+                aria-label="Close"
+            ></button>
+            <strong>Try logging in!</strong> More content visable when logged in.
+        </div>
+        <h1>Welcome to Arbitaja</h1>
+    </div>
+    <div class="container p-3">
+    </div>
 </template>
