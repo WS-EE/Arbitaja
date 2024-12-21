7d15f66529e86ecadeff9c408d2bd323cfa93692
Andreas Gabriel Vannas <jaanus.maddison@gmail.com>
2024-12-21 15:41:12 +0200

    feat: fade alerts in and out

--- app/frontend/src/views/homeView.vue
+++ app/frontend/src/views/homeView.vue
@@ -20,43 +20,71 @@ onMounted(async () => {
         console.log('User is unauthenticated')
         isLoggedIn.value = false
     }
+
+    // Fade alerts
+    setTimeout(() => {
+        alertShow.value = false
+    }, 5000)
 })
+
+const alertShow = ref(true)
+
+function closeAlert(){
+    alertShow.value = false
+}
 
 </script>
 
 <template>
     <div class="container p-3">
+        <!-- When user is logged out -->
         <div v-if="!isLoggedIn">
-            <div
-                class="alert alert-primary alert-dismissible fade show"
-                role="alert">
-                <button
-                    type="button"
-                    class="btn-close"
-                    data-bs-dismiss="alert"
-                    aria-label="Close"
-                ></button>
-                <strong>You are not authenticated!</strong> More content visable when logged in.
-            </div>
+            <Transition>
+                <div
+                    v-if="alertShow"
+                    class="alert alert-warning alert-dismissible"
+                    role="alert">
+                    <button
+                        @click="closeAlert"
+                        type="button"
+                        class="btn-close"
+                    ></button>
+                    <strong>You are not authenticated!</strong> More content visable when logged in.
+                </div>
+            </Transition>
             <h1>Welcome to Arbitaja</h1>
         </div>
+        <!-- When user is logged in -->
         <div v-if="isLoggedIn">
-            <div
-            class="alert alert-success alert-dismissible fade show"
-            role="alert">
-            <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="alert"
-                aria-label="Close"
-            ></button>
-            <strong>Success.</strong> Your are now authenticated and ready to use arbitaja.
-        </div>
-            
+            <Transition>
+                <div
+                    v-if="alertShow"
+                    class="alert alert-success alert-dismissible"
+                    role="alert">
+                    <button
+                        @click="closeAlert"
+                        type="button"
+                        class="btn-close"
+                    ></button>
+                <strong>Success.</strong> Your are now authenticated and ready to use arbitaja.
+                </div>
+            </Transition>
             <h1>Welcome {{ displayUsername }}!</h1>
             <p>You have logged in!</p>
-        </div>
+        </div>    
     </div>
     <div class="container p-3">
     </div>
 </template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
