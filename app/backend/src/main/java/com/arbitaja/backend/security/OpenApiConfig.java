cb4ce212b63da8a7cad0269ae3a67c10489d502d
HeinrichVannas <heinivannas@gmail.com>
2025-01-17 22:11:01 +0200

    feature: Now possible to test APIs from swagger UI when logged in from the frontend

--- app/backend/src/main/java/com/arbitaja/backend/security/OpenApiConfig.java
+++ app/backend/src/main/java/com/arbitaja/backend/security/OpenApiConfig.java
@@ -1,29 +1,24 @@
 package com.arbitaja.backend.security;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(title = "My API", version = "1.0", description = "API Documentation with Authentication"),
-        security = @SecurityRequirement(name = "basicAuth")
-)
-@SecurityScheme(
-        name = "basicAuth",
-        type = SecuritySchemeType.HTTP,
-        scheme = "basic",
-        in = SecuritySchemeIn.HEADER
-)
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI();
+        return new OpenAPI()
+                .info(new Info().title("My API").version("1.0"))
+                .addServersItem(new Server().url("http://localhost/api/v1"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("basicAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("basic")))
+                .addSecurityItem(new SecurityRequirement().addList("basicAuth"));
     }
 }
--- app/backend/src/main/java/com/arbitaja/backend/users/APIs/UserController.java
+++ app/backend/src/main/java/com/arbitaja/backend/users/APIs/UserController.java
@@ -203,5 +203,12 @@ public class UserController {
         public ErrorResponse(String error) {
             this.error = error;
         }
+        public String getError() {
+            return error;
+        }
+
+        public void setError(String error) {
+            this.error = error;
+        }
     }
 }
