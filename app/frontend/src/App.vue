7dbdca5c0ce152636cb8ee3ff28e537d0019f238
Andreas Gabriel Vannas <jaanus.maddison@gmail.com>
2025-01-18 22:29:05 +0200

    feat: add frontend not loading until backend is up

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -1,10 +1,27 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import customFooter from '@/components/generic/customFooter.vue';
+import axios from 'axios';
+import { onMounted,ref } from 'vue';
+const backendLoading = ref(true)
+
+onMounted(async () => {
+  let isLoading = true
+  while (isLoading){
+    const response = await axios.get('/health')
+    if (response.status === 200){
+      isLoading = false
+    } else {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+    }
+  }
+  backendLoading.value = false
+})
 </script>
 
 <template>
-  <div id="page-container">
+  <div v-if="backendLoading">Backend is loading up...</div>
+  <div v-else id="page-container"> 
     <div id="content-wrap">
       <RouterView />
     </div>
