4579a827f2d3dbc46c4bf6c6d0152534a192de87
Andreas Gabriel Vannas <jaanus.maddison@gmail.com>
2025-05-17 11:36:45 +0300

    refacto: change to devel-build
    Change to devel-build instead of devel as it is more clear on the
    frontend that this is the version not something else.

--- app/frontend/src/main.js
+++ app/frontend/src/main.js
@@ -21,7 +21,7 @@ const app = createApp(App);
 app.use(VueCookies, { expires: '7d'}, PrimeVue);
 
 // Set arbitaja version
-app.config.globalProperties.$arbitajaVersion = "devel"
+app.config.globalProperties.$arbitajaVersion = "devel-build"
 
 // Set copyright Header to use
 app.config.globalProperties.$copyrightHeader = "Copyright (c) 2025 WorldSkills Estonia"
